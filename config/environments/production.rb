b3d33b28a0a7b3ba22769fae21022f3a07828b1b
Jonathan Bowman <jonathanrbowman@me.com>
2014-01-12 18:30:23 -0500

    Added user table for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Powerprocedure::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://powerprocedure.herokuapp.com/' }
   
 end
